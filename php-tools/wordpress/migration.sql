2327e2e89671e180b501a02df0f45dba37a5c3c9
Carlos Fonseca <Carlos.Fonseca@gvcgroup.com>
2020-02-10 11:26:47 +0100

    Added wordpress migration sql script

---
+++ php-tools/wordpress/migration.sql
@@ -0,0 +1,13 @@
+
+-- REPLACE URLs AND RUN SQL STATEMENTS BELOW AS REQUIRED
+-- Check the tables prefix is correct!
+
+
+UPDATE wp_options SET option_value = replace(option_value, 'http://www.oldurl', 'http://www.newurl')
+WHERE option_name = 'home' OR option_name = 'siteurl';
+
+UPDATE wp_posts SET guid = replace(guid, 'http://www.oldurl','http://www.newurl');
+
+UPDATE wp_posts SET post_content = replace(post_content, 'http://www.oldurl', 'http://www.newurl');
+
+UPDATE wp_postmeta SET meta_value = replace(meta_value,'http://www.oldurl','http://www.newurl');
